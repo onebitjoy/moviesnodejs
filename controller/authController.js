d85e7442cf5fd7f0b0e4cc1521dcc3dab41b1d8f
abhishek <95994085+onebitjoy@users.noreply.github.com>
2024-01-16 20:20:44 +0530

    storing jwt tokens inside cookie

--- controller/authController.js
+++ controller/authController.js
@@ -9,6 +9,18 @@ const { compare } = pkg
 const createAndSendToken = async (user, statusCode, res) => {
   try {
     const token = await user.generateAuthToken()
+
+    const cookieOptions = {
+      expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+      httpOnly: true
+    }
+
+    if (process.env.NODE_ENV === "production") {
+      cookieOptions.secure = true
+    }
+
+    res.cookie('jwt', token, cookieOptions)
+
     res.status(statusCode).json({
       status: "success",
       token,
